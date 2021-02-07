d7a02a43fa69c8407fa4ba356cce9b6671c5c441
jesse1224 <jesse1224@users.noreply.github.com>
2021-02-07 11:15:59 +0000

    automated commit by check50 [check50=True]

--- population.c
+++ population.c
@@ -4,27 +4,29 @@
 int main(void)
 {
     // TODO: Prompt for start size
-     int n,y,b,d,end;
-  do
-  {
-      n = get_int("Start size: ");
-  } 
-  while (n < 9);
+    int n, y, b, d, end;
+    
+    do
+    
+    {
+        n = get_int("Start size: ");
+    } 
+    while (n < 9);
 
     // TODO: Prompt for end size
  
-  do
-  {
-      end = get_int("End size: ");
-  } 
-  while (end < n);
+    do
+    {
+        end = get_int("End size: ");
+    } 
+    while (end < n);
 
     // TODO: Calculate number of years until we reach threshold
     for (y = 0; n < end; y++)
     {
-    b = n/3;
-    d = n/4;
-    n = n + b - d;
+        b = n / 3;
+        d = n / 4;
+        n = n + b - d;  
     }
     
     // TODO: Print number of years
