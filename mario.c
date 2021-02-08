6d24e720598422f20dbfbd870431e700fdedb28c
jesse1224 <jesse1224@users.noreply.github.com>
2021-02-08 13:57:15 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -3,30 +3,30 @@
 
 void print(char c, int height);
 
-int main (void)
+int main(void)
 {
     int height;
     do
     {
-     height = get_int("Height: ");
+        height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
 
     for (int i = 0; i < height; i++)
     {
-     print(' ', height - 1 - i);
-     print('#', i + 1);
-     print(' ', 2);
-     print('#', i + 1);
+        print(' ', height - 1 - i);
+        print('#', i + 1);
+        print(' ', 2);
+        print('#', i + 1);
 
-     printf("\n");
+        printf("\n");
     }
 }
 
 void print(char c, int height)
 {
     for (int i = 0; i < height; i++)
-      {
+    {
         printf("%c", c);
-      }
+    }
 }
