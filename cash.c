0aa1c72910f424cef1abf27ad2bac10ddb3532e8
jesse1224 <jesse1224@users.noreply.github.com>
2021-02-08 15:50:04 +0000

    automated commit by check50 [check50=True]

---
+++ cash.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float dollars;
+    do
+    {
+        dollars = get_float("Change owed: ");
+    } while (dollars < 0);
+
+    int cents = round(dollars * 100);
+    int coins = 0;
+
+        while (cents >= 25)
+    {
+        cents -= 25;
+        coins++;
+    }
+        while (cents >= 10)
+    {
+        cents -= 10;
+        coins++;
+    }
+        while (cents >= 5)
+    {
+        cents -= 5;
+        coins++;
+    }
+        while (cents >= 1)
+    {
+        cents -= 1;
+        coins++;
+    }
+    printf("%i\n", coins);
+}
