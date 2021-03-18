8dcea0e03df136492329969417531b128b0818b7
jesse1224 <jesse1224@users.noreply.github.com>
2021-03-18 18:30:40 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -28,6 +28,6 @@ index = round(0.0588 * L - 0.296 * S - 15.8)
 if index < 1:
     print("Before Grade 1")
 elif index >= 16:
-    print ("Grade 16+")
+    print("Grade 16+")
 else:
     print(f"Grade {index}")
