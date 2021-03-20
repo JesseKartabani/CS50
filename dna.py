a2f51aaf55df7273fbb91740da533901cb6f0f1f
jesse1224 <jesse1224@users.noreply.github.com>
2021-03-20 01:15:44 +0000

    automated commit by check50 [check50=True]

--- dna.py
+++ dna.py
@@ -1,8 +1,8 @@
 import csv
 from sys import argv, exit
 
-# Maxium number of times a substring is repeated
-def max_substring(s, sub):
+
+def max_substring(s, sub):  # Maxium number of times a substring is repeated
     ans = [0] * len(s)
     for i in range(len(s) - len(sub), -1, -1):
         if s[i: i + len(sub)] == sub:
@@ -12,22 +12,25 @@ def max_substring(s, sub):
                 ans[i] = 1 + ans[i + len(sub)]
     return max(ans)        
 
-def dna_match(r, actual):
+
+def dna_match(r, actual):  # Checks for a DNA match
     for line in r:
         person = line[0]
         values = [int(val) for val in line[1:]]
         if values == actual:
             print(person)
             return
-    print("No match")
+    print("No match")      # Tells the user if theres no match
+
 
 def main():
-    if len(argv) != 3:
+    if len(argv) != 3:  # Making sure the user gives the correct input
         print("Usage: python dna.py data.csv sequence.txt")
         exit(1)
-        
+
+    # Uses the above functions to find if the DNA is a match to anyone in the csv file
     csv_path = argv[1]
-    with open(csv_path) as csv_file:
+    with open(csv_path) as csv_file: 
         r = csv.reader(csv_file)
         
         all_sequences = next(r)[1:]
@@ -39,5 +42,6 @@ def main():
             
         dna_match(r, actual)
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":  # Calls main
     main()
