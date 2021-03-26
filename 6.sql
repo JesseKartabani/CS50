980d011bdc8ebd7ed229cf74b2caad65c36c4e76
jesse1224 <jesse1224@users.noreply.github.com>
2021-03-26 18:15:33 +0000

    automated commit by check50 [check50=True]

--- 6.sql
+++ 6.sql
@@ -1,3 +1,3 @@
 -- In 6.sql, write a SQL query that lists the names of songs that are by Post Malone
 
-SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name - 'Post Malone');
+SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Post Malone');
