26eddde32a679729a08e3b17fa688110e92186c3
jesse1224 <jesse1224@users.noreply.github.com>
2021-03-27 11:57:12 +0000

    automated commit by check50 [check50=True]

--- 5.sql
+++ 5.sql
@@ -1,4 +1,4 @@
 -- In 5.sql, write a SQL query to list the titles and release years of all Harry Potter movies,
 -- in chronological order
 
-SELECT title, year FROM movies WHERE title = "%Harry Potter%";
+SELECT title, year FROM movies WHERE title like "Harry Potter%" ORDER BY year;
