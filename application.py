693920d102c9c518a6e76ada7cc30f9eb7d23d2e
jesse1224 <jesse1224@users.noreply.github.com>
2021-04-10 01:07:32 +0000

    automated commit by check50 [check50=True]

--- application.py
+++ application.py
@@ -221,15 +221,15 @@ def register():
     if request.method == "POST":
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
             
         # Ensure password is confirmed
         elif not request.form.get("confirmation"):
-            return apology("must confirm password", 403)
+            return apology("must confirm password", 400)
             
         # Ensure password and password confirmation match
         elif request.form.get("password") != request.form.get("confirmation"):
